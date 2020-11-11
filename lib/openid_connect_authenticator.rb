85abe677016650fb9fb0772ee8fcb679670fea86
David Taylor <david@taylorhq.com>
2020-11-11 18:46:11 +0000

    FIX: Gracefully handle errors while fetching the discovery document (#4)
    Previously an error loading the discovery document would raise an exception. Now, it will display an error to the user, and log the error for site admins to view at `/logs`. Specs are updated and improved accordingly.
    
    This moves the discovery document fetching out of OmniAuth and into Discourse. This makes it available for the upcoming rp-initiated-logout support.

--- lib/openid_connect_authenticator.rb
+++ lib/openid_connect_authenticator.rb
@@ -31,11 +31,42 @@ class OpenIDConnectAuthenticator < Auth::ManagedAuthenticator
     SiteSetting.openid_connect_overrides_email
   end
 
+  def discovery_document
+    document_url = SiteSetting.openid_connect_discovery_document.presence
+    if !document_url
+      oidc_log("No discovery document URL specified", error: true)
+      return
+    end
+
+    from_cache = true
+    result = Discourse.cache.fetch("openid-connect-discovery-#{document_url}", expires_in: 10.minutes) do
+      from_cache = false
+      oidc_log("Fetching discovery document from #{document_url}")
+      connection = Faraday.new { |c| c.use Faraday::Response::RaiseError }
+      JSON.parse(connection.get(document_url).body)
+    rescue Faraday::Error, JSON::ParserError => e
+      oidc_log("Fetching discovery document raised error #{e.class} #{e.message}", error: true)
+      nil
+    end
+
+    oidc_log("Discovery document loaded from cache") if from_cache
+    oidc_log("Discovery document is\n\n#{result.to_yaml}")
+
+    result
+  end
+
+  def oidc_log(message, error: false)
+    if error
+      Rails.logger.error("OIDC Log: #{message}")
+    elsif SiteSetting.openid_connect_verbose_logging
+      Rails.logger.warn("OIDC Log: #{message}")
+    end
+  end
+
   def register_middleware(omniauth)
 
     omniauth.provider :openid_connect,
       name: :oidc,
-      cache: lambda { |key, &blk| Rails.cache.fetch(key, expires_in: 10.minutes, &blk) },
       error_handler: lambda { |error, message|
         handlers = SiteSetting.openid_connect_error_redirects.split("\n")
         handlers.each do |row|
@@ -44,10 +75,7 @@ class OpenIDConnectAuthenticator < Auth::ManagedAuthenticator
         end
         nil
       },
-      verbose_logger: lambda { |message|
-        return unless SiteSetting.openid_connect_verbose_logging
-        Rails.logger.warn("OIDC Log: #{message}")
-      },
+      verbose_logger: lambda { |message| oidc_log(message) },
       setup: lambda { |env|
         opts = env['omniauth.strategy'].options
 
@@ -57,9 +85,7 @@ class OpenIDConnectAuthenticator < Auth::ManagedAuthenticator
         opts.deep_merge!(
           client_id: SiteSetting.openid_connect_client_id,
           client_secret: SiteSetting.openid_connect_client_secret,
-          client_options: {
-            discovery_document: SiteSetting.openid_connect_discovery_document,
-          },
+          discovery_document: discovery_document,
           scope: SiteSetting.openid_connect_authorize_scope,
           token_params: token_params,
           passthrough_authorize_options: SiteSetting.openid_connect_authorize_parameters.split("|")
